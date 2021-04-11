6fb2c957c0a94f0ff4e705e5e91f6693e1d419ac
nazarkorpal <nazarkorpal10@gmail.com>
2021-04-11 16:13:11 +0300

    created

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,18 @@ const path = require("path")
 
 connectDB()
 const app = express()
-app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(express.json({ extended: true }))
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "frontend", "public", "index.html"))
+    }).else
+}else {
+    app.get("/",(req, res)=>{
+        res.send("Api running")
+    })
+}
+
 app.use("/api/auth", require("./routes/userAuthRoutes"))
 app.use("/api/note", require("./routes/notesRoutes"))
 const PORT = process.env.PORT || 5000
