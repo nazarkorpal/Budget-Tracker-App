d1dde5b124dae91593b78688416ac9a3dcfed05d
nazarkorpal <nazarkorpal10@gmail.com>
2021-04-11 15:06:34 +0300

    created

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const connectDB = require("./config/db")
+const connectDB = require("./backend/config/db")
 
 connectDB()
 const app = express()
 app.use(express.static("public"))
 app.use(express.json({ extended: true }))
-app.use("/api/auth", require("./routes/userAuthRoutes"))
-app.use("/api/note", require("./routes/notesRoutes"))
+app.use("/api/auth", require("./backend/routes/userAuthRoutes"))
+app.use("/api/note", require("./backend/routes/notesRoutes"))
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {console.log(`Server running on port: ${PORT}`)})
